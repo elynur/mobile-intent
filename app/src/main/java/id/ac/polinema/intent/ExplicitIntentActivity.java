923b33f58993c6a16a72c3d8f3bfd1cd87fc82a3
elynur <elynurrahayu@gmail.com>
2020-02-18 09:04:21 +0700

    Explicit Intent Activity (.java)

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,34 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+    }
+
+
+    public void submit(View view){
+
+        //Cara pertama
+        //Menambahkan nilai bertipe string
+        String name = nameInput.getText().toString();
+        outputText.setText("Hello " + name );
+
+        //Cara Kedua
+//        outputText.setText("Hello " + nameInput.getText().toString() + " Congratulation");
     }
 
 }
